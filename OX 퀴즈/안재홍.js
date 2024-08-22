0716b855d2004834030f02f2127e951bf8410744
Jhong <ghddl23@naver.com>
2024-08-22 21:13:12 +0900

    edit OX 퀴즈/안재홍.js

--- OX 퀴즈/안재홍.js
+++ OX 퀴즈/안재홍.js
@@ -1,29 +1,21 @@
 function solution(quiz) {
     var answer = [];
-    let i = 0;
-    for(i = 0; i < quiz.length; i++){
-        let s = quiz[i].toString().split('=');
-        let a;
-        let b;
-        let c = parseInt(s[1]);
-        let ss;
+    
+    for(let i = 0; i < quiz.length; i++){
+        let s = quiz[i].toString().split(' ');
+        let a = parseInt(s[0]);
+        let b = s[1];
+        let c = parseInt(s[2]);
+        let d = parseInt(s[4]);
         
-        if(s[0].includes('+')){
-            ss = s[0].split('+');
-            a = parseInt(ss[0]);
-            b = parseInt(ss[1]);
-            console.log(a+" "+b+" "+c);
-            if(a+b===c) answer.push('O');
+        if(s[1]==='+'){
+            if(a+c===d) answer.push('O');
             else answer.push('X');
         }
-        else if(s[0].includes('-')){ 
-            ss = s[0].split('-');        
-            a = parseInt(ss[0]);
-            b = parseInt(ss[1]);
-            console.log(a+" "+b+" "+c);
-            if(a-b===c) answer.push('O');
+        else if(s[1]==='-'){ 
+            if(a-c===d) answer.push('O');
             else answer.push('X');
-        }   
+        } 
     }
     return answer;
 }
