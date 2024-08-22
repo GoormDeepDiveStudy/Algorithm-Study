107342603573124db0dd6ade852adfc12d78c718
Jhong <ghddl23@naver.com>
2024-08-22 18:34:47 +0900

    edit 옹알이(1)/안재홍.js

--- 옹알이 (1)/안재홍.js
+++ 옹알이 (1)/안재홍.js
@@ -2,26 +2,20 @@ function solution(babbling) {
     var answer = 0;
     const pronounce = ['aya', 'ye', 'woo', 'ma'];
     let i = 0;
-    for(i = 0; i < babbling.length; i++){
-        let s = babbling[i];
-        let arr;
-        let a = 1;
-        let j = 0;
-        while(j<s.length){
-            if(s.indexOf(j)==='a'&&s.indexOf(j+1)==='y'&&s.indexOf(j+2)==='a')
-                j += 3;
-            else if(s.indexOf(j)==='w'&&s.indexOf(j+1)==='o'&&s.indexOf(j+2)==='o')
-                j += 3;
-            else if(s.indexOf(j)==='y'&&s.indexOf(j+1)==='e')
-                j += 2;
-            else if(s.indexOf(j)==='m'&&s.indexOf(j+1)==='a')
-                j+= 2;
-            else{
-                a = 0;
-                break;
+    for(i = 0; i < babbling.length; i++){  
+        for(let j = 0; j < pronounce.length; j++){
+            while(true){
+                if(babbling[i].includes(pronounce[j]))
+                    babbling[i] = babbling[i].replace(pronounce[j]," ");
+                else break;
             }
         }
-        answer += a;
+        while(true){
+            if(babbling[i].includes(" "))
+               babbling[i] = babbling[i].replace(" ","");
+            else break;
+        }
+        if(babbling[i]==="") answer++;
     }
     return answer;
 }
