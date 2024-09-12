eb649a6bada65a62c720dd45879669005ecb9311
Jhong <ghddl23@naver.com>
2024-09-12 14:54:20 +0900

    [프로그래머스] 콜라 문제 : 안재홍.js

---
+++ 콜라 문제/안재홍.js
@@ -0,0 +1,12 @@
+function solution(a, b, n) {
+  var answer = 0;
+  let blank = 0;
+  
+  while(n >= a){
+      answer += b * Math.floor(n / a);
+      blank = n % a;
+      n = b * Math.floor(n / a) + blank; 
+  }
+  
+  return answer;
+}
