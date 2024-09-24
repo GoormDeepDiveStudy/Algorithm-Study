31cca6e5c74a15f02c1829e565889e144c980370
Jhong <ghddl23@naver.com>
2024-09-24 15:11:51 +0900

    [프로그래머스] 소수 만들기 : 안재홍.js

---
+++ 소수 만들기/안재홍.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+  let list = [];
+  for(let i = 0; i < nums.length - 2; i++){
+      for(let j = i + 1; j < nums.length - 1; j++){
+          for(let k = j + 1; k < nums.length; k++){
+              let n = nums[i] + nums[j] + nums[k];
+              
+              for(let l = 2; l < n; l++){
+                  if(n % l === 0) {
+                      break;
+                  }else if(l === n - 1){
+                      list.push(n);
+                  }
+              }
+          }
+      }
+  }
+  
+  return list.length;
+}
